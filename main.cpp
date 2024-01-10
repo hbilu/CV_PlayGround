063596101823748b809ec662adeb8c294c21ec17
hbilu <hacerbilu@gmail.com>
2024-01-10 22:02:34 +0100

    opencv capture live video

--- main.cpp
+++ main.cpp
@@ -1,21 +1,30 @@
-#include <iostream>
+
 #include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <stdio.h>
+#include <iostream>
 
 int main(int argc, char** argv) {
-    cv::VideoCapture cap("../Virtual-Paint.mp4");
+    cv::VideoCapture cap;
+    int deviceID = 0;
+    int apiID = cv::CAP_ANY;
+    cap.open(deviceID, apiID);
     if (!cap.isOpened()) {
-        std::cerr << "Error: could not open video file" << std::endl;
+        std::cerr << "ERROR! Unable to open camera" << std::endl;
         return -1;
     }
     cv::namedWindow("Video", 720);
+    cv::Mat frame;
 
     while (true) {
-        cv::Mat frame;
         cap.read(frame);
         if (frame.empty()) {
+            std::cerr << "ERROR! blank frame grabbed" << std::endl;
             break;
         }
-        cv::imshow("Video", frame);
+        cv::imshow("Live", frame);
         if (cv::waitKey(30) >= 0) {
             break;
         }
